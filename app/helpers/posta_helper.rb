bb429337a18a08bd334c6cddabee1f373b73723c
tetsupon <te.ka.20230305@gmail.com>
2024-02-16 20:12:25 +0900

    first commit

---
+++ app/helpers/posta_helper.rb
@@ -0,0 +1,2 @@
+module PostaHelper
+end
---
+++ test/controllers/posta_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PostaControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
